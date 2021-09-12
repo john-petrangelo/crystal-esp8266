2fd30d394eacae0e314df2fe727a97df60b04da0
John Petrangelo <john@petrangelo.org>
2021-09-11 22:52:32 -0400

    Create dummy secrets.h file

---
+++ secrets.h
@@ -0,0 +1,10 @@
+////////////////////////////////////////////////////////////////////////////
+// Change SECRET_HOST to the hostname you'd like your device to respond to.
+// You will be able to connect to the web interface of your device using
+// the hostname <SECRET_HOST>.local.
+#define SECRET_HOST "crystal"
+
+////////////////////////////////////////////////////////////////////////////
+// Set the name and password for your WiFi network here.
+#define SECRET_SSID "MyNetworkName"
+#define SECRET_PASSWORD "MyPassword"
