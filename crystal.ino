371b45bc736c135ea77dddac7782a2709e2d1e1a
John Petrangelo <>
2021-12-30 13:54:16 -0500

    Moved network code to separate module

--- crystal.ino
+++ crystal.ino
@@ -1,12 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
-#include <ArduinoOTA.h>
 #include <Adafruit_NeoPixel.h>
 
-#include "src/lumos-arduino/lumos-arduino/Actions.h"
 #include "src/lumos-arduino/lumos-arduino/Logger.h"
-#include "src/lumos-arduino/lumos-arduino/Lumos.h"
 #include "src/lumos-arduino/lumos-arduino/Patterns.h"
 
 // Secrets are defined in another file called "secrets.h" to avoid commiting secrets
@@ -35,12 +31,11 @@ Color pixels[PIXELS_COUNT];
 
 ModelRunner modelRunner;
 
-void setup(void) {
-  setupSerial();
-  setupWiFi();
-  setupHTTP();
-  setupMDNS();
-  setupOTA();
+void setup() {
+  Serial.begin(115200);
+  Serial.println("");
+
+  setupNetwork();
 
   logServer.begin();
 
@@ -51,13 +46,9 @@ void setup(void) {
 //  lumos.runForever(&crystalLight);
 }
 
-int count = 0;
-
-void loop(void) {
+void loop() {
   // Check for network activity.
-  server.handleClient();
-  MDNS.update();
-  ArduinoOTA.handle();
+  loopNetwork();
 
   // Animate the LEDs. 
   //  lumos.loop();
@@ -66,117 +57,6 @@ void loop(void) {
   strip.show();
 
   loopLogger();
-}
-
-// Setup serial communications channel
-void setupSerial() {
-  Serial.begin(115200);
-  Serial.println("");
-}
-
-// Get the device setup on the local network
-void setupWiFi() {
-  // Setup WiFi
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(SECRET_SSID, SECRET_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-
-  // If we recognize the MAC address, use a different hostname specific to that MAC address
-  String macAddress = WiFi.macAddress();
-  if (macAddress == "E8:DB:84:98:7F:C3") {
-    hostname = "shard";
-  } else if (macAddress = "84:CC:A8:81:0A:53") {
-    hostname = "crystal";
-  } else {
-    hostname = WiFi.hostname();
-  }
-  Serial.print("Network: ");
-  Serial.println(SECRET_SSID);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("MAC address: ");
-  Serial.println(WiFi.macAddress());
-  Serial.print("Hostname: ");
-  Serial.println(hostname);
-}
-
-// Setup the web server and handlers
-void setupHTTP() {
-  server.on("/", HTTP_GET, handleRoot);
-  server.on("/status", HTTP_GET, handleStatus);
-//  server.on("/off", HTTP_GET, handleOff);
-//  server.on("/darkcrystal", HTTP_GET, handleDarkCrystal);
-//  server.on("/flame", HTTP_GET, handleFlame);
-//  server.on("/rainbow", HTTP_GET, handleRainbow);
-  server.onNotFound(handleNotFound);
-  server.begin();
-  Serial.println("HTTP server started");
-}
-
-// Setup an MDNS responder so we can be found by <host>.local instead of IP address
-void setupMDNS() {
-  if (MDNS.begin("hostname")) {
-    Serial.println("MDNS responder started: crystal.local");
-  }
-}
-
-// Setup OTA updates
-void setupOTA() {
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
-
-  // Hostname defaults to esp8266-[ChipID]
-  ArduinoOTA.setHostname(hostname.c_str());
-
-  // No authentication by default
-  // ArduinoOTA.setPassword((const char *)"123");
-
-  ArduinoOTA.onStart([]() {
-    Serial.println("OTA Start");
-
-    Patterns::setSolidColor(strip, pixels, BLACK);
-    Patterns::setSolidColor(pixels, strip.numPixels()-1, strip.numPixels(), WHITE);
-    Patterns::applyPixels(strip, pixels);
-    strip.show();
-  });
-  
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("OTA Progress: %u%%\r", (progress / (total / 100)));
-
-    int otaPixels = progress / (total / strip.numPixels());
-    Serial.printf("progress: %u  total: %u  otaPixels: %u\r", progress, total, otaPixels);
-    Patterns::setSolidColor(pixels, 0, otaPixels, GREEN);
-    Patterns::applyPixels(strip, pixels);
-    strip.show();
-  });
-  
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nOTA End");
-
-    Patterns::setSolidColor(strip, pixels, BLACK);
-    Patterns::applyPixels(strip, pixels);
-    strip.show();
-  });
-  
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("OTA Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("OTA Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("OTA Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("OTA Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("OTA Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("OTA End Failed");
-
-    Patterns::setSolidColor(strip, pixels, RED);
-    Patterns::applyPixels(strip, pixels);
-    strip.show();
-  });
-  
-  ArduinoOTA.begin();
-  Serial.println("OTA ready");
 }
 
 // Check to see if the network logger needs to be setup or torn down
---
+++ network.ino
@@ -0,0 +1,122 @@
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+
+// One-stop to set up all of the network components
+void setupNetwork() {
+  setupWiFi();
+  setupHTTP();
+  setupMDNS();
+  setupOTA();
+}
+
+// Get the device setup on the local network
+void setupWiFi() {
+  // Setup WiFi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SECRET_SSID, SECRET_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  // If we recognize the MAC address, use a different hostname specific to that MAC address
+  String macAddress = WiFi.macAddress();
+  if (macAddress == "E8:DB:84:98:7F:C3") {
+    hostname = "shard";
+  } else if (macAddress = "84:CC:A8:81:0A:53") {
+    hostname = "crystal";
+  } else {
+    hostname = WiFi.hostname();
+  }
+  Serial.print("Network: ");
+  Serial.println(SECRET_SSID);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("MAC address: ");
+  Serial.println(WiFi.macAddress());
+  Serial.print("Hostname: ");
+  Serial.println(hostname);
+}
+
+// Setup the web server and handlers
+void setupHTTP() {
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/status", HTTP_GET, handleStatus);
+//  server.on("/off", HTTP_GET, handleOff);
+//  server.on("/darkcrystal", HTTP_GET, handleDarkCrystal);
+//  server.on("/flame", HTTP_GET, handleFlame);
+//  server.on("/rainbow", HTTP_GET, handleRainbow);
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+// Setup an MDNS responder so we can be found by <host>.local instead of IP address
+void setupMDNS() {
+  if (MDNS.begin("hostname")) {
+    Serial.println("MDNS responder started: crystal.local");
+  }
+}
+
+// Setup OTA updates
+void setupOTA() {
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname(hostname.c_str());
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("OTA Start");
+
+    Patterns::setSolidColor(strip, pixels, BLACK);
+    Patterns::setSolidColor(pixels, strip.numPixels()-1, strip.numPixels(), WHITE);
+    Patterns::applyPixels(strip, pixels);
+    strip.show();
+  });
+  
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("OTA Progress: %u%%\r", (progress / (total / 100)));
+
+    int otaPixels = progress / (total / strip.numPixels());
+    Serial.printf("progress: %u  total: %u  otaPixels: %u\r", progress, total, otaPixels);
+    Patterns::setSolidColor(pixels, 0, otaPixels, GREEN);
+    Patterns::applyPixels(strip, pixels);
+    strip.show();
+  });
+  
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nOTA End");
+
+    Patterns::setSolidColor(strip, pixels, BLACK);
+    Patterns::applyPixels(strip, pixels);
+    strip.show();
+  });
+  
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("OTA Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("OTA Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("OTA Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("OTA Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("OTA Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("OTA End Failed");
+
+    Patterns::setSolidColor(strip, pixels, RED);
+    Patterns::applyPixels(strip, pixels);
+    strip.show();
+  });
+  
+  ArduinoOTA.begin();
+  Serial.println("OTA ready");
+}
+
+void loopNetwork() {
+  // Check for network activity.
+  server.handleClient();
+  MDNS.update();
+  ArduinoOTA.handle();  
+}
