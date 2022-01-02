25d1eae85899e51b1edac8ea8be060dab88e4b26
John Petrangelo <>
2022-01-01 23:32:53 -0500

    Added useful utility functions fmap and lerp

---
+++ utils.ino
@@ -0,0 +1,26 @@
+/**
+ * Re-maps a number from one range to another. That is, a value of fromLow would get mapped to toLow,
+ * a value of fromHigh to toHigh, values in-between to values in-between, etc.
+ *
+ * This is taken directly from the Arduino map function described here:
+ * https://www.arduino.cc/reference/en/language/functions/math/map/
+ * 
+ * ...except it has been re-applied to floats instead of longs.
+ */
+float fmap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+/**
+ * Returns an interpolation between two inputs (v0, v1) for a parameter (t)
+ * 
+ * This is taken directly from the sample code on the Wikipedia page:
+ * https://en.wikipedia.org/wiki/Linear_interpolation#Programming_language_support
+ *
+ * Imprecise method, which does not guarantee v = v1 when t = 1, due to floating-point arithmetic error. This method is monotonic
+ * This form may be used when the hardware has a native fused multiply-add instruction.
+ *
+ */
+float lerp(float v0, float v1, float t) {
+  return v0 + t * (v1 - v0);
+}
