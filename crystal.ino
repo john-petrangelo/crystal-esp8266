2cbe6836186502ef3a76208f1c57472fb3d7dc55
John Petrangelo <>
2022-01-03 22:41:23 -0500

    Use the new MapModel, added some duration logging to help diagnose gradual slowing down bug

--- crystal.ino
+++ crystal.ino
@@ -42,15 +42,35 @@ void setup() {
   strip.show(); // Initialize all pixels to 'off'
 
 //  GradientModel *gm = new GradientModel("gradient", 8, RED, VIOLET, INDIGO, BLUE, GREEN, YELLOW, ORANGE, RED);
-  GradientModel *grad_left = new GradientModel("grad-left", 5, RED, YELLOW, RED, YELLOW, RED);
-  GradientModel *grad_right = new GradientModel("grad-right", 5, BLUE, GREEN, BLUE, GREEN, BLUE);
-  RotateModel *rot_left = new RotateModel("rotate down", 0.2, RotateModel::DOWN, grad_left);
-  RotateModel *rot_right = new RotateModel("rotate up", 0.2, RotateModel::UP, grad_right);
-  WindowModel *window = new WindowModel("window", 0.0, 0.5, rot_left, rot_right);
-  modelRunner.setModel(window);
+//  GradientModel *grad_left = new GradientModel("grad-left", 5, RED, YELLOW, RED, YELLOW, RED);
+//  GradientModel *grad_right = new GradientModel("grad-right", 5, BLUE, GREEN, BLUE, GREEN, BLUE);
+//  RotateModel *rot_left = new RotateModel("rotate down", 0.2, RotateModel::DOWN, grad_left);
+//  RotateModel *rot_right = new RotateModel("rotate up", 0.2, RotateModel::UP, grad_right);
+//  WindowModel *window = new WindowModel("window", 0.0, 0.5, rot_left, rot_right);
+//  modelRunner.setModel(window);
+
+  GradientModel *grad_left = new GradientModel("grad-left", 2, RED, YELLOW);
+  GradientModel *grad_right = new GradientModel("grad-right", 2, BLUE, GREEN);
+
+  RotateModel *rot_left = new RotateModel("rotate down", 4.0, RotateModel::DOWN, grad_left);
+  RotateModel *rot_right = new RotateModel("rotate up", 1.0, RotateModel::UP, grad_right);
+
+  MapModel *map_left = new MapModel("map left", 0.0, 0.2, 0.0, 1.0, rot_left);
+  MapModel *map_right = new MapModel("map right", 0.2, 1.0, 0.0, 1.0, rot_right);
+
+  WindowModel *window = new WindowModel("window", 0.0, 0.2, map_left, map_right);
+
+  RotateModel *rot_window = new RotateModel("rotate window", 0.5, RotateModel::DOWN, window);
+  
+  modelRunner.setModel(rot_window);
 }
 
 void loop() {
+  static int lastUpdateMS = millis();
+  long const logDurationIntervalMS = 1000;
+
+  long now = millis();
+  
   // Check for network activity.
   loopNetwork();
 
@@ -59,7 +79,17 @@ void loop() {
   Patterns::applyPixels(strip, pixels);
   strip.show();
 
+  long nowAfterPixels = millis();
+
   loopLogger();
+
+  long nowAfterLogger = millis();
+
+  if (now - lastUpdateMS >= logDurationIntervalMS) {
+    lastUpdateMS = now;
+    Logger::logf("Completed loop, handling LEDs took %dms, updating the logger took %dms, total duration %dms\n",
+      nowAfterPixels - now, nowAfterLogger - nowAfterPixels, nowAfterLogger - now);
+  }
 }
 
 // Check to see if the network logger needs to be setup or torn down
