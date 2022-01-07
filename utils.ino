9e7efbd3d50887f464d685eb2153d954949a02c1
John Petrangelo <>
2022-01-06 22:05:34 -0500

    Added comment for frand function

--- utils.ino
+++ utils.ino
@@ -11,6 +11,9 @@ float fmap(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+/**
+ * Return a random number between fmin and fmax.
+ */
 float frand(double fmin, double fmax) {
   return fmin + random(1UL << 31) * (fmax - fmin) / (1UL << 31);
 }
